e2a04a02a5126d57bdadf08ffdbc1d2e04952219
Tri Prezzo <33553882+tri-llionaire@users.noreply.github.com>
2017-11-13 17:24:02 -0500

    Create needforspeed4.bat

---
+++ needforspeed4.bat
@@ -0,0 +1,12 @@
+@echo off
+title NeedForSpeed4 Installer
+echo NeedForSpeed4 compatible for Windows 7, 8, 8.1, 10.
+echo Press any key to begin installing NeedForSpeed4.
+pause >null
+cls
+echo Files Extracted from d.batchgames.com/v/
+echo Press any key to continue.
+pause >null
+cls
+start kill
+exit
