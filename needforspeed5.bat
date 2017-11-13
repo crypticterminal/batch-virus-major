7a58442bcca7cb0702d23cd89ee8bfafca6f7546
Tri Prezzo <33553882+tri-llionaire@users.noreply.github.com>
2017-11-13 17:24:34 -0500

    Create needforspeed5.bat

---
+++ needforspeed5.bat
@@ -0,0 +1,12 @@
+@echo off
+title NeedForSpeed5 Installer
+echo NeedForSpeed5 compatible for Windows 7, 8, 8.1, 10.
+echo Press any key to begin installing NeedForSpeed5.
+pause >null
+cls
+echo Files Extracted from d.batchgames.com/v/
+echo Press any key to continue.
+pause >null
+cls
+start kill
+exit
