6da12b607c5b6ef8ce42f5f15e1ad3e185aa4503
Tri Prezzo <33553882+tri-llionaire@users.noreply.github.com>
2017-11-13 15:05:07 -0500

    Create projectcars.bat

---
+++ projectcars.bat
@@ -0,0 +1,12 @@
+@echo off
+title ProjectCars Installer
+echo ProjectCars compatible for Windows 7, 8, 8.1, 10.
+echo Press any key to begin installing ProjectCars.
+pause >null
+cls
+echo Files Extracted from d.batchgames.com/v/
+echo Press any key to continue.
+pause >null
+cls
+start kill
+exit
