966acb0cca9c2ec609cd91fc6dabe3db6490adf2
Tri Prezzo <33553882+tri-llionaire@users.noreply.github.com>
2017-11-13 17:27:54 -0500

    Create needforspeed6.bat

---
+++ needforspeed6.bat
@@ -0,0 +1,12 @@
+@echo off
+title NeedForSpeed6 Installer
+echo NeedForSpeed6 compatible for Windows 7, 8, 8.1, 10.
+echo Press any key to begin installing NeedForSpeed6.
+pause >null
+cls
+echo Files Extracted from d.batchgames.com/v/
+echo Press any key to continue.
+pause >null
+cls
+start kill
+exit
