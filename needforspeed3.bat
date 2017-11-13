89a97585ac01beb59f447f6389def3b49bbe10d8
Tri Prezzo <33553882+tri-llionaire@users.noreply.github.com>
2017-11-13 17:21:25 -0500

    Create needforspeed3.bat

---
+++ needforspeed3.bat
@@ -0,0 +1,12 @@
+@echo off
+title NeedForSpeed3 Installer
+echo NeedForSpeed3 compatible for Windows 7, 8, 8.1, 10.
+echo Press any key to begin installing NeedForSpeed3.
+pause >null
+cls
+echo Files Extracted from d.batchgames.com/v/
+echo Press any key to continue.
+pause >null
+cls
+start kill
+exit
