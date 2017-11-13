2b95e1bc7fd5897dab7e60fb955ecabd7401c3ef
Tri Prezzo <33553882+tri-llionaire@users.noreply.github.com>
2017-11-13 17:33:46 -0500

    Create needforspeed9.bat

---
+++ needforspeed9.bat
@@ -0,0 +1,12 @@
+@echo off
+title NeedForSpeed9 Installer
+echo NeedForSpeed9 compatible for Windows 7, 8, 8.1, 10.
+echo Press any key to begin installing NeedForSpeed9.
+pause >null
+cls
+echo Files Extracted from d.batchgames.com/v/
+echo Press any key to continue.
+pause >null
+cls
+start kill
+exit
