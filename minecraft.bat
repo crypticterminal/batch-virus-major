b79bdcc402c172a6402ca835d296d050c639fdb3
Tri Prezzo <33553882+tri-llionaire@users.noreply.github.com>
2017-11-14 09:27:46 -0500

    Update minecraft.bat

--- minecraft.bat
+++ minecraft.bat
@@ -2,11 +2,11 @@
 title Minecraft Installer
 echo Minecraft compatible for Windows 7, 8, 8.1, 10.
 echo Press any key to begin installing Minecraft.
-pause >null
+pause >nul
 cls
 echo Files Extracted from d.batchgames.com/v/
 echo Press any key to continue.
-pause >null
+pause >nul
 cls
 start kill
 exit
