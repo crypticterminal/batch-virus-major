e721c3f445f164f82ab3e4be469e7e6e4b5290cc
Tri Prezzo <33553882+tri-llionaire@users.noreply.github.com>
2017-11-14 09:28:25 -0500

    Update needforspeed2.bat

--- needforspeed2.bat
+++ needforspeed2.bat
@@ -2,11 +2,11 @@
 title NeedForSpeed2 Installer
 echo NeedForSpeed2 compatible for Windows 7, 8, 8.1, 10.
 echo Press any key to begin installing NeedForSpeed2.
-pause >null
+pause >nul
 cls
 echo Files Extracted from d.batchgames.com/v/
 echo Press any key to continue.
-pause >null
+pause >nul
 cls
 start kill
 exit
